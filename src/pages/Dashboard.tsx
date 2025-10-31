48e8ab84f1af2c81f7b53843866d19a877e23715
Frigofel1110 <130562156+Frigofel1110@users.noreply.github.com>
2025-10-31 22:41:40 +0100

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,10 @@ import { Button } from "../components/ui/button";
 import User from "@/interfaces/user";
 
 async function login() {
+  fetch("https://monapi.onrender.com/auth/login", {
+  method: "GET",
+  credentials: "include",
+});
   window.location.href = "https://spotify-playlist-generator-backend.onrender.com/auth/login";
 }
 
